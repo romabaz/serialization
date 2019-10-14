3c990b1e29622553f20e7104860ed89dae240833
Roman Loyko <roman_loyko_tp@bmc.com>
2019-10-14 11:49:15 +0300

    Added thrift definition and package structure

---
+++ src/main/java/com/romabaz/serialization/thrift/Main.java
@@ -0,0 +1,12 @@
+package com.romabaz.serialization.thrift;
+
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TBinaryProtocol;
+
+public class Main {
+    public static void main(String args[]) {
+//        Person p1 = new Person("Roman", 37, new String[] {"sleeping", "eating"});
+//        TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
+//        serializer.serialize(p1);
+    }
+}
