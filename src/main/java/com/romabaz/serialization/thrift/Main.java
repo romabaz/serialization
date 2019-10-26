aaad3e9a045629250c1b3151ab762d9dd7e6616e
Roman Loyko <roman_loyko_tp@bmc.com>
2019-10-26 10:50:02 +0300

    optimized code

--- src/main/java/com/romabaz/serialization/thrift/Main.java
+++ src/main/java/com/romabaz/serialization/thrift/Main.java
@@ -4,18 +4,21 @@ import com.romabaz.serialization.thrift.definitions.Person;
 import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String args[]) throws TException, IOException {
+    public static void main(String[] args) throws TException, IOException {
         Person p1 = new Person("Roman", 37, Arrays.asList("sleeping", "eating"));
-        TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
-        byte[] personBytes = serializer.serialize(p1);
-        FileOutputStream fos = new FileOutputStream("person_thrift.bytes");
-        fos.write(personBytes);
-        fos.close();
+        TSerializer serializerBinary = new TSerializer(new TBinaryProtocol.Factory());
+        TSerializer serializerCompact = new TSerializer(new TCompactProtocol.Factory());
+        try (FileOutputStream fosBinary = new FileOutputStream("person_thrift_binary.bytes");
+             FileOutputStream fosCompact = new FileOutputStream("person_thrift_compact.bytes")) {
+            fosBinary.write(serializerBinary.serialize(p1));
+            fosCompact.write(serializerCompact.serialize(p1));
+        }
     }
 }
