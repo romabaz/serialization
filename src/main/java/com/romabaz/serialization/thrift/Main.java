6bbfdcb5481c29b0845723492a30beee569c3650
Roman Loyko <roman_loyko_tp@bmc.com>
2019-10-26 09:08:22 +0300

    Implemented working serialization in thrift

--- src/main/java/com/romabaz/serialization/thrift/Main.java
+++ src/main/java/com/romabaz/serialization/thrift/Main.java
@@ -1,12 +1,21 @@
 package com.romabaz.serialization.thrift;
 
+import com.romabaz.serialization.thrift.definitions.Person;
+import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TBinaryProtocol;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String args[]) {
-//        Person p1 = new Person("Roman", 37, new String[] {"sleeping", "eating"});
-//        TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
-//        serializer.serialize(p1);
+    public static void main(String args[]) throws TException, IOException {
+        Person p1 = new Person("Roman", 37, Arrays.asList("sleeping", "eating"));
+        TSerializer serializer = new TSerializer(new TBinaryProtocol.Factory());
+        byte[] personBytes = serializer.serialize(p1);
+        FileOutputStream fos = new FileOutputStream("person_thrift.bytes");
+        fos.write(personBytes);
+        fos.close();
     }
 }
