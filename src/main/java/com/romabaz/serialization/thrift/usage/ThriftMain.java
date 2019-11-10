8ae7de8041aec5abee45c3eb4c572ca3bcd9bd8b
Roman Loyko <roman_loyko_tp@bmc.com>
2019-11-10 14:47:19 +0200

    Fixed thrift build

---
+++ src/main/java/com/romabaz/serialization/thrift/usage/ThriftMain.java
@@ -0,0 +1,24 @@
+package com.romabaz.serialization.thrift.usage;
+
+import com.romabaz.serialization.thrift.generated.Person;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ThriftMain {
+    public static void main(String[] args) throws TException, IOException {
+        Person p1 = new Person("Roman", 37, Arrays.asList("sleeping", "eating"));
+        TSerializer serializerBinary = new TSerializer(new TBinaryProtocol.Factory());
+        TSerializer serializerCompact = new TSerializer(new TCompactProtocol.Factory());
+        try (FileOutputStream fosBinary = new FileOutputStream("person_thrift_binary.bytes");
+             FileOutputStream fosCompact = new FileOutputStream("person_thrift_compact.bytes")) {
+            fosBinary.write(serializerBinary.serialize(p1));
+            fosCompact.write(serializerCompact.serialize(p1));
+        }
+    }
+}
