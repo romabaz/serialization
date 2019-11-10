e0d2fe308f6b55d0c73569754964af970eca8f86
Roman Loyko <roman_loyko_tp@bmc.com>
2019-11-10 14:28:46 +0200

    implemented protoc step as maven plugin

--- src/main/java/com/romabaz/serialization/protobuf/usage/ProtobufMain.java
+++ src/main/java/com/romabaz/serialization/protobuf/usage/ProtobufMain.java
@@ -1,6 +1,7 @@
 package com.romabaz.serialization.protobuf.usage;
 
-import com.romabaz.serialization.protobuf.definitions.PersonOuterClass;
+import com.romabaz.serialization.protobuf.generated.Person2OuterClass;
+import com.romabaz.serialization.protobuf.generated.Person3OuterClass;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,13 +9,20 @@ import java.util.Arrays;
 
 public class ProtobufMain {
     public static void main(String[] args) throws IOException {
-        PersonOuterClass.Person.Builder personBuilder = PersonOuterClass.Person.newBuilder();
-        PersonOuterClass.Person person = personBuilder.setName("Roman")
+        Person3OuterClass.Person3 person3 = Person3OuterClass.Person3.newBuilder()
+                .setName("Roman")
                 .setAge(37)
                 .addAllInterests(Arrays.asList("sleeping", "eating"))
                 .build();
-        try (FileOutputStream fosBinary = new FileOutputStream("person_proto3.bytes")) {
-            person.writeTo(fosBinary);
+        Person2OuterClass.Person2 person2 = Person2OuterClass.Person2.newBuilder()
+                .setName("Roman")
+                .setAge(37)
+                .addAllInterests(Arrays.asList("sleeping", "eating"))
+                .build();
+        try (FileOutputStream fosBinary = new FileOutputStream("person_proto3.bytes");
+             FileOutputStream fosBinary2 = new FileOutputStream("person_proto2.bytes")) {
+            person3.writeTo(fosBinary);
+            person2.writeTo(fosBinary2);
         }
     }
 }
