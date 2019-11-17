7db38d413453919e321ef365525668b2d8107cda
Roman Loyko <roman_loyko_tp@bmc.com>
2019-11-17 23:05:04 +0200

    Removed unused import

--- src/main/java/com/romabaz/serialization/avro/usage/AvroMain.java
+++ src/main/java/com/romabaz/serialization/avro/usage/AvroMain.java
@@ -6,7 +6,6 @@ import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumWriter;
 
 import java.io.File;
