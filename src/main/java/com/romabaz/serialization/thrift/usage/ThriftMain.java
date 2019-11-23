1ca8b2a8020c402b86c89a647722f3dd82ae8848
Roman Loyko <roman_loyko_tp@bmc.com>
2019-11-23 18:39:51 +0200

    Added simple json for thrift

--- src/main/java/com/romabaz/serialization/thrift/usage/ThriftMain.java
+++ src/main/java/com/romabaz/serialization/thrift/usage/ThriftMain.java
@@ -5,6 +5,7 @@ import org.apache.thrift.TException;
 import org.apache.thrift.TSerializer;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TJSONProtocol;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,10 +22,13 @@ public class ThriftMain {
         p1.setInterests(Arrays.asList("sleeping", "eating"));
         TSerializer serializerBinary = new TSerializer(new TBinaryProtocol.Factory());
         TSerializer serializerCompact = new TSerializer(new TCompactProtocol.Factory());
+        TSerializer serializerJson = new TSerializer(new TJSONProtocol.Factory());
         try (FileOutputStream fosBinary = new FileOutputStream(BASE_OUTPUT_DIR + "/person_thrift_binary.bytes");
-             FileOutputStream fosCompact = new FileOutputStream(BASE_OUTPUT_DIR + "/person_thrift_compact.bytes")) {
+             FileOutputStream fosCompact = new FileOutputStream(BASE_OUTPUT_DIR + "/person_thrift_compact.bytes");
+             FileOutputStream fosJson = new FileOutputStream(BASE_OUTPUT_DIR + "/person_thrift_json.bytes")) {
             fosBinary.write(serializerBinary.serialize(p1));
             fosCompact.write(serializerCompact.serialize(p1));
+            fosJson.write(serializerJson.serialize(p1));
         }
     }
 }
